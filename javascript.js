469ec8e9e759d0539dbbd770d9e0ff8408fc66f7
Hunter Yeago <hyradar@tutamail.com>
2022-10-26 09:47:08 +1100

    functional read/unread button

--- javascript.js
+++ javascript.js
@@ -1,9 +1,23 @@
 let myLibrary = [];
 
+let readbutton = document.querySelector('.readbutton');
+readbutton.addEventListener("click", changeRead);
+
 function Book() {
     //the constructor
 }
 
 function addBookToLibrary() {
     //do stuff here
+}
+
+function changeRead (){
+    let btn = document.querySelector('.readbutton');
+
+    if (btn.innerHTML === 'Read') {
+        btn.innerHTML = 'Unread';
+    }
+    else {
+        btn.innerHTML = 'Read';
+    }
 }
