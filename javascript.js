eca0398604faaf0b787e854a610a4c460d595eb2
Hunter Yeago <hyradar@tutamail.com>
2022-10-26 11:00:42 +1100

    added addBookToLibrary basic functionality and changeStatus functionality (with the array itself, not HTML)

--- javascript.js
+++ javascript.js
@@ -1,9 +1,7 @@
 let myLibrary = [];
 
-let theHobbit = Object.create(Book.prototype);
-theHobbit.name = 'The Hobbit';
-theHobbit.author = 'J.R.R. Tolkien';
-theHobbit.status = 'Read';
+let readbutton = document.querySelector('.readbutton');
+// readbutton.addEventListener("click", createRow());
 
 function Book(name, author, status) {
     this.name = name;
@@ -11,28 +9,33 @@ function Book(name, author, status) {
     this.status = status;
 }
 
+const theHobbit = Object.create(Book.prototype);
+theHobbit.name = 'The Hobbit';
+theHobbit.author = 'J.R.R. Tolkien';
+theHobbit.status = 'Read';
+
+
 Book.prototype.addBookToLibrary = function() {
-    //do stuff here
+    myLibrary.push(this);
 }
 
 Book.prototype.changeStatus = function() {
 
+    if (this.status === "Read") {
+        this.status = "Unread";
+    }
+    else {
+        this.status = "Read";
+    }
 }
 
-let readbutton = document.querySelector('.readbutton');
-readbutton.addEventListener("click", createRow);
 
-// Book.prototype.createRow = function() {
-//     let myTable = document.querySelector('.bookshelf');
-//     let row = myTable.appendChild(tr);
-//     row.createElement(td); 
-//     row.createElement(td);
-//     row.createElement(td);
-//     row.createElement(td);
-//     readbutton.style.color = 'red';
-// }
+theHobbit.addBookToLibrary();
+console.log(myLibrary);
 
-function createRow() {
+
+// Proof of concept for adding row
+/* function createRow() {
 
     // Access to Button
     let btn = document.querySelector('.readbutton');
@@ -49,45 +52,12 @@ function createRow() {
 
     // Add Row 
     const newRow = myTable.appendChild(rowNode);
-    newRow.className = 'newrow';
-
-    let x = document.querySelector('.newrow');
-    x.append(bookData, authorData, statusData, deleteData);
+    
+    newRow.append(bookData, authorData, statusData, deleteData);
     bookData.innerHTML = "book";
     authorData.innerHTML = "author";
     statusData.innerHTML = "status";
     deleteData.innerHTML = "delete";
-
-    // Add Data
-    // let nameData = x.appendChild(dataNode);
-    // let authorData = x.appendChild(dataNode);
-    // let statusData = x.appendChild(dataNode);
-    // let deleteData = x.appendChild(dataNode);
-    
-    // nameData.className = "newBook";
-    // nameData.innerHTML = 'The Hobbit';
-    
-    // authorData.className = "author";
-    // authorData.innerHTML = 'J.R.R. Tolkien';
-    
-    // statusData.className = "status";
-    // statusData.innerHTML = 'Read';
-    
-    // deleteData.className = "delete";
-    // deleteData.innerHTML = 'Delete';
     
 }
-
-
-
-
-// function changeStatus() {
-//     let btn = document.querySelector('.readbutton');
-
-//     if (btn.innerHTML === 'Read') {
-//         btn.innerHTML = 'Unread';
-//     }
-//     else {
-//         btn.innerHTML = 'Read';
-//     }
-// }
+*/
